1e796f9ef1be59a10fb033588fc58c485b93c2ef
Caio Giffoni <caio_cgf@hotmail.com>
2023-02-25 16:58:32 -0300

    moved to app to deploy to docker hub

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
+# 
 FROM python:3.9
 
 # 
@@ -10,7 +11,7 @@ COPY ./requirements.txt /code/requirements.txt
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
 # 
-COPY . /code/
+COPY ./app /code/app
 
 # 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "80"]
---
+++ app/main.py
@@ -0,0 +1,72 @@
+import joblib
+import pandas as pd
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from . import new_vars
+
+app = FastAPI()
+
+class Body(BaseModel):
+    property_type_Bed_and_breakfast: int
+    property_type_Apartment: int
+    property_type_Condominium: int
+    property_type_Guest_suite: int
+    property_type_Guesthouse: int
+    property_type_Hostel: int
+    property_type_House: int
+    property_type_Loft: int
+    property_type_Other: int
+    property_type_Serviced_apartment: int
+    room_type_Entire_home_apt: int
+    room_type_Hotel_room: int
+    room_type_Private_room: int
+    room_type_Shared_room: int
+    cancellation_policy_flexible: int
+    cancellation_policy_moderate: int
+    cancellation_policy_strict: int
+    cancellation_policy_strict_14_with_grace_period: int
+    latitude: float
+    longitude: float
+    accommodates: int
+    bathrooms: int
+    bedrooms: int
+    beds: int
+    extra_people: int
+    minimum_nights: int
+    year: int
+    month: int
+    amenities: int
+    host_listings_count: int
+    host_is_superhost: int
+    instant_bookable: int 
+
+
+
+@app.post("/predict")
+async def predict_values(body: Body):
+    body_dict = body.dict()
+    body_df = pd.DataFrame(body_dict, index=[0])
+    values_x = pd.DataFrame(new_vars.ddd, index=[0])
+    body_df = body_df.rename(columns={
+        "property_type_Bed_and_breakfast": "property_type_Bed and breakfast", 
+        "property_type_Guest_suite": "property_type_Guest suite", 
+        "property_type_Serviced_apartment": "property_type_Serviced apartment",
+        "room_type_Entire_home_apt" :"room_type_Entire home/apt", 
+        "room_type_Hotel_room":"room_type_Hotel room", 
+        "room_type_Private_room":"room_type_Private room", 
+        "room_type_Shared_room":"room_type_Shared room"
+    })
+    body_df = body_df[new_vars.index]
+    xxx = pd.DataFrame(new_vars.dddxxx, index=[0])[new_vars.index]
+    values_x_new  = values_x[new_vars.index]
+    model = joblib.load('app/model.joblib')
+    price = model.predict(body_df)
+    price1 = model.predict(values_x_new)
+    price2 = model.predict(xxx)
+
+    return {
+        '0': price[0],
+        "message": price1[0],
+       "message2": price2[0]
+    }
